e20fc6dd7bd1a0dd2f85b70afa6b5f8e5e4c9d6f
mgladczak <mateusz@gladczak.com>
2026-04-05 17:31:25 +0200

    update
    Signed-off-by: mgladczak <mateusz@gladczak.com>

--- scripts/nrTransformOptimaPayload.py
+++ scripts/nrTransformOptimaPayload.py
@@ -3,31 +3,10 @@ import json
 import sys
 
 def main():
-    if len(sys.argv) < 2:
-        print(json.dumps({"error": "No input data in sys.argv[1]"}))
-        sys.exit(1)
+    print("Script Payload:")
 
-    raw = sys.argv[1].strip()
-
-    try:
-        rows = json.loads(raw)
-    except Exception as e:
-        print(json.dumps({"error": f"Incorrect JSON: {e}"}))
-        sys.exit(1)
-
-    result = {}
-
-    for row in rows:
-        nip = row.get("Baz_Nip")
-        if not nip:
-            continue
-
-        result[nip] = {
-            "nazwa": row.get("Baz_Nazwa"),
-            "baza": row.get("Baz_NazwaBazy")
-        }
-
-    print(json.dumps(result, ensure_ascii=False))
+    payload = json.loads(sys.argv[1])
+    print(json.dumps(payload, indent=4))
 
 if __name__ == "__main__":
     main()
